30572eb234496a6b6bac0581b156b85cd198f96f
Jamond Turner <jamondturner@yahoo.com>
2022-03-27 20:08:37 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+// const chai = 
+// const spies =
+
+
+// receivesAFunction
+// take a callback function as an argument
+// call the callback function
+// it doesn't matter what this function returns, so long as it calls the callback function
+
+function receivesAFunction(){
+console.log("Eat your wheaties!");
+console.log("Take your vitamins");
+}
+
+
+
+// returnsANamedFunction
+// take no arguments
+// return a named function
+
+function returnsANamedFunction(){
+
+}
+
+// returnsAnAnonymousFunction
+// take no arguments
+// return an anonymous function
+
+function returnsAnAnonymousFunction(){
+    console.log("Get To the Paper!")
+}
+
