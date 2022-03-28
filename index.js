a432ddb434321e7190e4090e720a87eacb030016
Jamond Turner <jamondturner@yahoo.com>
2022-03-27 22:29:44 -0400

    Completed Assignments

--- index.js
+++ index.js
@@ -7,6 +7,30 @@
 // call the callback function
 // it doesn't matter what this function returns, so long as it calls the callback function
 
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(){
+
+};
+// create array of drivers return the first two
+
+const returnLastTwoDrivers = function returnLastTwoDrivers(){
+
+
+}
+//create array of drivers return the last two
+
+// create array
+
+selectingDrivers = [function returnFirstTwoDrivers(){}, function returnLastTwoDrivers(){}]
+
+function createFareMultiplier(){}
+
+const fareDoubler = 
+
+const fareTripler =
+
+
+function selectDifferentDrivers(){}
+
 function receivesAFunction(){
 console.log("Eat your wheaties!");
 console.log("Take your vitamins");
